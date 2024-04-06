c1d2942782c411b0413d7b79c49748b0c5d85e15
xiaoqihfut <xiaoqihfut@gmail.com>
2024-04-06 16:31:13 +0800

    xx

--- test/function.cpp
+++ test/function.cpp
@@ -47,8 +47,19 @@ int main()
     // auto plus = [=] (int a, int b) -> int { return x + y + a + b; };
     // int c = plus(1, 2);
     // std::cout << c << "\n";
+
+    // 值捕获时，编译器会把捕获到的值作为类的成员变量，并且变量是以值的方式传递的。
+    // 需要注意的时，如果所有的参数都是值捕获的方式，那么生成的operator()函数是const函数的，
+    // 是无法修改捕获的值的，哪怕这个修改不会改变lambda表达式外部的变量，
+    // 如果想要在函数内修改捕获的值，需要加上关键字 mutable。向下面这样的形式。
+    // https://zhuanlan.zhihu.com/p/384314474
+    // int x = 1; int y = 2;
+    // auto plus = [=] (int a, int b) mutable -> int { x++; return x + y + a + b; };
+    // int c = plus(1, 2);
+    // std::cout << c << " " << x <<"\n"; // 7 1
+
     int x = 1; int y = 2;
-    auto plus = [=] (int a, int b) mutable -> int { x++; return x + y + a + b; };
+    auto plus = [&] (int a, int b) -> int { x++; return x + y + a + b;};
     int c = plus(1, 2);
     std::cout << c << " " << x <<"\n";
     return EXIT_SUCCESS;
